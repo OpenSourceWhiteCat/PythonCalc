04b9810d383bab81937699e849d6ed4e51b3dbb8
OpenSourceWhiteCat <gamerwild38@gmail.com>
2025-07-28 04:03:17 +0530

    adding calculator to repo

---
+++ PythonCalculator.py
@@ -0,0 +1,20 @@
+a = float(input("enter the first number "))
+b = float(input("enter the second number "))
+
+op = input("enter the oprator (+,-,*,/)")
+
+if op == '+':
+    print("answer",a+b)
+elif op == '-':
+    print("answer",a-b) 
+elif op == '*':
+    print("answer",a*b)
+elif op == '/':
+    print("answer",a/b)  
+
+
+
+
+
+
+
